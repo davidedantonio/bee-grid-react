010d830dbd54c0de5e023530ec9415f1198bced1
davide <davide.dantonio@webeetle.com>
2018-06-13 12:42:48 +0200

    lINK

--- src/docs/components/MainComponents/Header.js
+++ src/docs/components/MainComponents/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Grid from '@webeetle/Grid';
 import GridItem from '@webeetle/GridItem';
 
